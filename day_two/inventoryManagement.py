d0e33f2dda66ce02cf65cfe90b4cec571c0615d1
Jacob Bracken <Jacob@Jacobs-MacBook-Pro.local>
2018-12-02 16:12:04 +0000

    day two

---
+++ day_two/inventoryManagement.py
@@ -0,0 +1,314 @@
+files = '''tjxmoewpqkyaiqvmndgflunszc
+tjxmobwpqkyaihvrndgfjubszm
+tjxmzewpqkyaihvrydgflrbszc
+tjxmoeypqkyvihvrndgflubsza
+tjcmoewpqkytihvrndgflgbszc
+tjvmoewpqkyanevrndgflubszc
+tjxmoewpqkdiihirndgflubszc
+tjxboewpqkyaihbrnogflubszc
+ojpmoewpqkyaihvjndgflubszc
+tjxyoewpqkyaiuvrndgflutszc
+tjxmoewpqkyalhvrndmflebszc
+tjxmoewpqzyaihhrndgflubszf
+tjxmrewpqkyaihirndgfjubszc
+pjxmoewpqkyaihvendgfbubszc
+txxmkewpqkyjihvrndgflubszc
+tjxmoewcqkyaihvrnmgflubczc
+tjxmoewkqkyaghvrndgfluvszc
+tjxmoewfqkhaihvrndgflubhzc
+jjqmoewpqkyaihvrndzflubszc
+tjxmoewmqksaihvcndgflubszc
+tjrmoewpqkyaihvrvdgflubzzc
+tjxxoewpqkyaiiwrndgflubszc
+cjxmoawxqkyaihvrndgflubszc
+tjxdoewpvkyaihvrndgflubsoc
+tjxmsewpqkyaihvrndgfluzozc
+tjxmoewpqkyafhvrnyeflubszc
+tjxmlewpqkyawhvondgflubszc
+tjxmonwpqkyaiqvrnxgflubszc
+tjxmoewcqkyaihvrnjgflumszc
+tjvmoewpqkyaihveadgflubszc
+tjxmogfpqkyaigvrndgflubszc
+tybmoewpqkyaihvrndgllubszc
+tjxmoewpdkyaihvrndgfluwbzc
+etxmbewpqkyaihvrndgflubszc
+tjxmoeapqcynihvrndgflubszc
+tbxmoewpqkyaihvrndgfdebszc
+haxmoewpqyyaihvrndgflubszc
+ojxmoewpqkyaihvrnegflubszr
+tjxmoewpqkyaihvrndoflubarc
+ljxmoewpqkykihvrndgflvbszc
+tjxmovwpqkyaihvrndgfluzsyc
+tvxmoewpqkyanhvrkdgflubszc
+tjxmoewpqkyaihkrndgfluwwzc
+zjxmoewpfkyaihvrndgfrubszc
+tjxyoegpqkyaihvrndlflubszc
+tjxmoewpqkyamhvrnsgflubmzc
+tjmmoewpqkyaihvrndgftuwszc
+tjxmoewpqbraihvrncgflubszc
+tjxmeeepqkyainvrndgflubszc
+tjemoegpqkyaihvredgflubszc
+tjxmoewpqkyaihvdndgfzubqzc
+tjxmoegrqkyaihfrndgflubszc
+tjxmoewpqxyaihvrndgfluyvzc
+qjxmoewpqkyaiwvrnfgflubszc
+tjxwoewpqkyashkrndgflubszc
+tjzmoewiqkyaihurndgflubszc
+tjumuewpqkyaihvrndgflubssc
+tyxooewpukyaihvrndgflubszc
+tjxvoewpqkyaiivindgflubszc
+ijxmoqwpqkyaihvradgflubszc
+tjxmlewpqkyaihvrhdgflubwzc
+tjxmkewpqkyajhqrndgflubszc
+tjxmoewpqkqaiherndgflurszc
+tjamoewpqkyaizvondgflubszc
+tjxgogwpqkyalhvrndgflubszc
+tjxmoewpqkyachvrndgflubuzq
+tjxmowqpqkyaihvrnegflubszc
+mjxmoewpwkyaihvrndgfkubszc
+tpbmoewpqkyaihvrzdgflubszc
+tjbmoewpqkyaiuvrndgflsbszc
+tjxmoewpqklaghvrndgflubazc
+tjxmoewpqkyrihvrndgwlpbszc
+tjcmoewpqksaiyvrndgflubszc
+tjxmoeapqkymihvindgflubszc
+tjxmdewpqkyafhvrndgflqbszc
+tjxmoewpqxyaihvrndsflubszi
+tjxmoeppqkyaihvrcdgflubszd
+tjxmomwpqkyainvrmdgflubszc
+tjxmovwpqkyaihvrndgfdubdzc
+tjxmoewwqkiaihvrjdgflubszc
+tmxmoewpqkyaifvrndgflubszs
+tbxmoewpqkyaihvrbdgflunszc
+tjxmoewrqkyaihvxndgflubszp
+ujxmoewpqkyaihvxndgflubpzc
+tdxmotwpqkyaihvdndgflubszc
+tjxmvewpqkyaihfrndgtlubszc
+tjfmoewpqkyaihvrnyqflubszc
+tjxfolwzqkyaihvrndgflubszc
+ojrmoiwpqkyaihvrndgflubszc
+tjsmoqwpqkyqihvrndgflubszc
+tjxmohwpqkyaihvrudgflubslc
+tjxtoiwpqkyaihvrnogflubszc
+taxmoewpqkyaiyvrndgfwubszc
+tjxwnezpqkyaihvrndgflubszc
+tjxmyevpqkyaivvrndgflubszc
+tjxdoeopqkyaihvgndgflubszc
+tjxaoewpqkmaihvrndgflufszc
+tjxmoewpqkyaxhvrndgflubncc
+tjxmoewpqkyaihurndgflubbjc
+tjxmjewpqgyaihvrnngflubszc
+tjxmogwpqkyaihvrndgflubbcc
+tjxmoewplkyaihvrnpgflibszc
+tjwmoewpqkyaohvrndgfbubszc
+tjwmoewpqkyaihvrndgfsubszm
+tjxmogwpqkyaihvrndiflubqzc
+tjxmoewpqkyaihvrndgflopshc
+rjxmlewpvkyaihvrndgflubszc
+tjxmogwpakyaihvrndgflzbszc
+tjxmoeppqkyaihvrndgflmxszc
+tjxmoewpqkyhihgrndgfzubszc
+tjxqoewpqkyaihtrndgwlubszc
+tjxnoespqkyaihvrndgflubsuc
+tjmmoewpqkraihvrndgflfbszc
+tjxmoewnqkwaihvrndgflubstc
+tjxmoewpqqyaihvrndgfljbszi
+tjxmoewpqkyaihkrkdgalubszc
+tjxmoewpqkyaihvradgjlurszc
+tvxmoewpqkybihvrndbflubszc
+tjxvoewpqkyaihvradgfoubszc
+tjxmoewpqfyaihvlodgflubszc
+tjxmoewmnkyaiivrndgflubszc
+kjxmoewpqkyaihprndgflcbszc
+hjxmoewpqkcaihvrndgvlubszc
+tjxmoewcqkyaihvrncgfllbszc
+tuxmoewpckyaihvrndoflubszc
+tjxmdewpokyaihvrndgflubszn
+mjxmaewpqkyaqhvrndgflubszc
+tjxmoewpmzyaihvrndgfiubszc
+tjxmoewnqkyvihvrndgflubszk
+tjxmoewpmnyaihvrndgftubszc
+zjxmoewpqkysihvrndgfmubszc
+tjxmoewpqkyaihzrntgflubbzc
+tjxmoewpqkgaihwrndsflubszc
+tjxjoewpqkyaihvrndgflgbizc
+oqxmoewpqkyaihvrndgfldbszc
+wjamoewpqkyaihvfndgflubszc
+tjxmoewtmkyvihvrndgflubszc
+tjlmojwpqkyaihvrndgfludszc
+tjxmowwpqkyaihvrndefludszc
+tjxmoewpqkbaihvrndgfluaszt
+tjxmoewpqkzaahvrodgflubszc
+tjxmoewpqkgaihvrndgflubtpc
+tjxmoenpqkyaihcrndgfqubszc
+tbxmoewpqbyaihvrndgalubszc
+tjvmoewqqkyaihvrndvflubszc
+tjxmoewpqkeaihvundgfaubszc
+txxmoewpqkyaihvrwdgflpbszc
+tzxmoewpqkijihvrndgflubszc
+tjxmoewoqkytiuvrndgflubszc
+tjxmrejplkyaihvrndgflubszc
+tjxmoewpqkynihvrpxgflubszc
+tjxmoewpqkvanhvrndgvlubszc
+tjxmoewpdkyiihvrndgflubszs
+tpxmyewpqkyaihvrndgfeubszc
+tpxmoewpqyyaihvrndhflubszc
+tjsmoewpqkyaihvrndhflubnzc
+tjxmoewpukyaihvrnmgflubwzc
+txxmoewpqlyaihwrndgflubszc
+tjxmoewprkyaiovrndgflubxzc
+tjxmouwpqkyaihzrodgflubszc
+tjxmojwpqkywimvrndgflubszc
+tjxsoewpqkyaihvrzdgqlubszc
+tfxmoewpakyaihvrndgllubszc
+tjhmoewpqiyaihvrndgflubsac
+tjxmoewpqkoaihvrndoflubsxc
+tjxmoewpqkyzpjvrndgflubszc
+tjxmoewpqkyaiharndgzlnbszc
+tjimoevpqkyaihvrndgflubbzc
+tjxsoewpqkyahhvrndgfzubszc
+txxmoewpqkyaimvrrdgflubszc
+tjxmoewpwkyaihvrndpylubszc
+tjxmoewpskyaghvrndgfbubszc
+tjxmuewpqmyaihvrndgfyubszc
+tjxmoewpqkyaihvdndgglubsxc
+xjxmoewpqkyjiovrndgflubszc
+gjxmoewpqkyaihvrndodlubszc
+tjbmoewpqkyaihvridgflvbszc
+tjxmozwpqkyapbvrndgflubszc
+tjxeoewpqkyqihvrndgflubhzc
+tjxdoewpqzyaihvrndgflubsmc
+tjxmwewpqkyathvcndgflubszc
+tjxmoewpszyaihvrndgflusszc
+tuxmoewpqkyaihvrndgfluasxc
+tjemoewpnvyaihvrndgflubszc
+tjxmoewpjkyaihvrndgjlufszc
+tjomoewppkyaihvzndgflubszc
+tjxmvewpqkyaimvrntgflubszc
+rjxmoewpqkyaihvpndgflubszq
+hjxzoewpqkyaihvridgflubszc
+texmoejpqkyaihvrndgflubszx
+tjxcoewpqkyaihbrxdgflubszc
+tjxmoewpnkyaihvrndgfltbsze
+tjxmoewpdkyaihvrndwfluwbzc
+tjxmoewpqryjihkrndgflubszc
+tjlmoewpqkhaihvrndgflubsnc
+tjxmovapqkjaihvrndgflubszc
+tjxvoewpqkyaihqrndgfluyszc
+tjxwoewnqkyaihvrndgfgubszc
+tjdmoewpqklaihvcndgflubszc
+tjxmoewpvkynihvrndgflubskc
+tjxmtewpqkyaihvhndgfluaszc
+tjxmoewpqkyanhvrnpgfluvszc
+tjxmoewpqkyaifvbndgflubspc
+tjxmoexpqknaihvrndgxlubszc
+qjxmoewqqkyaihvrndgflubpzc
+tjxmoewppkyaihvaxdgflubszc
+myxmoewpqkyaihvrudgflubszc
+tjxmwewpmkyaihvrndgflubssc
+tjxmoewpqkyaihvrndgfxqbszq
+tjxmoewhqkyaahvrndgflubbzc
+tbxmoewmqkyaihvrndgflubszu
+toxmolwpqkyaihvrndnflubszc
+tjxmoewhqkyaihvrnrgflubvzc
+yjxmoewcqkyaihvrndgflubfzc
+tjxmoewpqkyamhvrgdgflmbszc
+tjxmtewpqkyaizvrndgfluoszc
+tjxmoewpqzyaihvrntsflubszc
+fjxmoewpqkyaihyrmdgflubszc
+tjxwoewpqcyaihbrndgflubszc
+tjxmoebpqkzaihvrndcflubszc
+tjxmoewpqkyaihvrndnlmubszc
+tjxmoewpqkyaihvrndgeyubskc
+tfxmoewpqryaihvrndgfluiszc
+tjxmoewpqkjaihvynngflubszc
+tjxmoewpqkqaihvonjgflubszc
+tjfmokwpqkyeihvrndgflubszc
+djxmoewpkkyaihvrnmgflubszc
+tjxmiewpqkyaihvrndgflubhlc
+tjxmmejpqkyaihvrnhgflubszc
+djxmoewmqkyaihvrnggflubszc
+tjxmoewpqkyaihvrkggflubsze
+gjxmoewpqkyaihjrndgflvbszc
+tjxmoewpqkyaidvrndgkzubszc
+tjxmoewpqkyaedvrnpgflubszc
+sjxmoewpqkyaihvrnngfluhszc
+tjxmoewpqkuaihvrndghlubxzc
+tjxmoewgqkyuihvrndgftubszc
+tjxmoewpqsyaifvrkdgflubszc
+tjxrrewpqkyaihvrnpgflubszc
+tjxmoezpqkyaihvrwdgflabszc
+tjfmoewpqknaihvrndgflubkzc
+tjxmoewnqkxaihvrndgflubtzc
+tjxmoewpkkyaihvrndgfrnbszc
+tjxmorwpnkqaihvrndgflubszc
+tsxmoewwqkyathvrndgflubszc
+tjxmoeupqkyaihvrndyflubszp
+bjxmoewdqkyaihvrndgflurszc
+tjxmoewpvkyaihvrndoflubszq
+sjxmoewpqkyaihvrndgflubyec
+tjxmoewpqkyaizcrndgfnubszc'''.strip().split('\n')
+
+totalsDict = {'twoCount': 0, 'threeCount': 0}
+
+
+def maxLetters(boxID):
+    found = {
+        'twos': False,
+        'threes': False
+    }
+    letterSet = set(boxID)
+    for letter in letterSet:
+        count = countLetter(letter, boxID)
+        if (count == 2 and not found['twos']):
+            found['twos'] = True
+            totalsDict['twoCount'] += 1
+        elif (count == 3 and not found['threes']):
+            found['threes'] = True
+            totalsDict['threeCount'] += 1
+        if (found['twos'] and found['threes']):
+            break
+
+
+def countLetter(letter, fullWord):
+    count = 0
+    for i in fullWord:
+        if i == letter:
+            count += 1
+    return count
+
+def compareStrings(string1, string2):
+    wrongChars = 0
+    for i in range(len(string1)):
+        if not (string1[i] == string2[i]):
+            wrongChars += 1
+        if wrongChars > 1:
+            return False
+    return True
+        
+
+def findSimilarId(fileIds):
+    similar = []
+    for i in range(len(fileIds)):
+        for j in range(i+1, len(fileIds)):
+            if (compareStrings(fileIds[i], fileIds[j])):
+                similar.append(fileIds[i])
+                similar.append(fileIds[j])
+
+    return similar
+
+def removeDifferences(stringsArray):
+    basestring = ''
+    for i in range(len(stringsArray[0])):
+        if stringsArray[0][i] == stringsArray[1][i]:
+            basestring += stringsArray[0][i]
+
+    return basestring
+
+def main():
+    for contentID in files:
+        maxLetters(contentID)
+    print(totalsDict['twoCount'] * totalsDict['threeCount'])
+    print(removeDifferences(findSimilarId(files)))
+
+main()
